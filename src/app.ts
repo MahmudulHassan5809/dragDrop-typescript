6b1219009bb084a0151c61907d27a956d1b664e0
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2022-04-22 00:19:40 +0600

    finishing drag & drop[

--- src/app.ts
+++ src/app.ts
@@ -62,6 +62,18 @@ class ProjectState extends State<Project>{
     addProject(title: string, description:string, numOfPeople: number){
         const newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active)
         this.projects.push(newProject);
+        this.updateListeners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId);
+        if(project){
+            project.status = newStatus;
+            this.updateListeners();
+        }
+    }
+
+    private updateListeners(){
         for (const listenersFn of this.listeners){
             listenersFn(this.projects.slice());
         }
@@ -219,8 +231,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
         }
     } 
 
+    @autobind
     dropHandler(event: DragEvent){
-        console.log(event)
+        const prjId = event.dataTransfer!.getData('text/plain');
+        projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished)
     }
 
     @autobind
